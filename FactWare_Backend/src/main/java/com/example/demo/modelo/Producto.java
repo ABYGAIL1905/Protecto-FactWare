cb3e21636d7f28a56ccfb3de5e01dcaa233850c8
CristhianEPC <cristhian.pallmay.est@tecazuay.edu.ec>
2023-02-08 17:19:14 -0500

    Modificacion de productos

--- FactWare_Backend/src/main/java/com/example/demo/modelo/Producto.java
+++ FactWare_Backend/src/main/java/com/example/demo/modelo/Producto.java
@@ -34,9 +34,25 @@ private static final long serialVersionUID = 1L;
 	
 	 @Column (name = "nombre_producto")
 	private String nombre_producto;
-	
-	 @Column (name = "precio")
-	private double precio;
+         
+        @Column (name = "descripcion_producto")
+	private String descripcion_producto;
+        
+        @Column (name = "codigoBarras_producto")
+	private int codigoBarras_producto;
+        
+        @Column (name = "costo_producto")
+	private Double costo_producto;
+        
+        @Column (name = "pvp_producto")
+	private Double pvp_producto;
+        
+        @Column (name = "utilidad_producto")
+	private Double utilidad_producto;
+        
+        @Column (name = "estadoIVA_producto")
+	private String estadoIVA_producto;
+
 	 @JsonIgnore
 	    @OneToMany(mappedBy = "producto")
 	    List<DetalleFactura> detalle_factura;
