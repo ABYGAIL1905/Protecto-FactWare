0157004f04a8a0d1845d30ebd88f21c95ee680a2
Fernada1 <maria.andino.est@tecazuay.edu.ec>
2023-02-22 12:17:35 -0500

    creacion base mafer

--- FactWare_Backend/src/main/java/com/example/demo/service/IProductoService.java
+++ FactWare_Backend/src/main/java/com/example/demo/service/IProductoService.java
@@ -1,5 +1,24 @@
 package com.example.demo.service;
 
-public interface IProductoService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+
+import com.example.demo.modelo.Producto;
+
+
+
+public interface IProductoService {
+	
+
+	 public List<Producto> findAll();
+	    
+	    public Producto save(Producto producto);
+	    
+	   public Producto findById(Long id);
+	   
+	   public void delete(Long id);
 }
