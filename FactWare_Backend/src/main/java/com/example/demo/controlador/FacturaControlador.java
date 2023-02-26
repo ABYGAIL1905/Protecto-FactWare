8757e3f9c892769a4a5b768f5595bd941e51e8e0
dianasaico20 <diana.saico.est@tecazuay.edu.ec>
2023-02-25 20:33:56 -0500

    Arreglo de los controladores

--- FactWare_Backend/src/main/java/com/example/demo/controlador/FacturaControlador.java
+++ FactWare_Backend/src/main/java/com/example/demo/controlador/FacturaControlador.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,7 +37,7 @@ public class FacturaControlador {
     }
 
     //Guardar
-    @PutMapping("/savF")
+    @PostMapping("/savF")
     @ResponseStatus(HttpStatus.CREATED)
     public Factura save(@RequestBody Factura factura) {
         return sF.save(factura);
